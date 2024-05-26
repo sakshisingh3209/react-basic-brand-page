dd2f8cf4a691267d9c894c0149963a129da101bb
Sakshi Singh <sakshisingh3209@gmail.com>
2024-05-26 22:36:04 +0530

    first commit

---
+++ brand-page/src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import Navigation from './components/Navigation';
+import HeroSection from './components/HeroSection';
+const App = () => {
+    return <div>
+<Navigation/>
+ <HeroSection/>
+    </div>
+}
+export default App;
